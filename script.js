27837a8e24edb36910a556b7a6492af4f1dc4fd9
Ansh Vishnoi <114725557+anshvishnoi04@users.noreply.github.com>
2025-04-06 16:58:52 +0530

    Update script.js
    Update

--- script.js
+++ script.js
@@ -10,16 +10,14 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Starboy - The Weeknd", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Demons -vImagine Dragons", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Mouth of the river - Imagine Dragons", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Ghost Stories - Coldplay", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Sparks - Coldplay", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Viva La Vida - Coldplay", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Hymn for the weekend - Coldplay", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Pain - Ryan Jones", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
